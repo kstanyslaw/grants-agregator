9937b9cff04e04f524d2141513f5dcd59e4e8032
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2018-01-26 21:51:27 +0700

    Update app.module and app. component So all of my components can work

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from "@angular/http";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { TableComponent } from './table/table.component';
+import { ManualyAddComponent } from './manualy-add/manualy-add.component';
+import { ModalComponent } from './manualy-add/modal/modal.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        NavbarComponent,
+        TableComponent,
+        ManualyAddComponent,
+        ModalComponent
     ],
     imports: [
         BrowserModule,
+        FormsModule,
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]

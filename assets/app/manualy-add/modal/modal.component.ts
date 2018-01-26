2a8a824698168849928be657f87c7a2965628862
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2018-01-26 21:50:36 +0700

    Add modal component to manualy-add Big plans about it. Imagine it like a dialog window with forms

---
+++ assets/app/manualy-add/modal/modal.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'modal-app',
+  templateUrl: 'modal.component.html',
+})
+export class ModalComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {}
+}

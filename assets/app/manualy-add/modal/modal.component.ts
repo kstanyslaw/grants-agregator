6f96c460b5bec945bd2b25979ff7c8291345fc2f
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2018-02-01 17:47:05 +0700

    Add worked modal Can see modal, close, add grant Can't call modal if it's not shown

--- assets/app/manualy-add/modal/modal.component.ts
+++ assets/app/manualy-add/modal/modal.component.ts
@@ -3,8 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'modal-app',
   templateUrl: 'modal.component.html',
+  styles:[`
+      .modal {
+        background-color: rgba(0, 0, 0, 0.6);
+      }
+
+      .show {
+        display: block;
+      }
+    `]
 })
 export class ModalComponent implements OnInit {
+
+  isShow = true;
+
   constructor() {  }
 
   ngOnInit() {}

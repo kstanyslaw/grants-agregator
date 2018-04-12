4945e41bdc2fa0eb4f104c090831455ad4b4e599
Stanislav <k.stanyslaw@gmail.com>
2018-04-12 16:53:49 +0700

    Detect what grant user select in table

--- assets/app/grants-search/grant-details/grant-details.component.ts
+++ assets/app/grants-search/grant-details/grant-details.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'grant-details-app',
   templateUrl: 'grant-details.component.html',
 })
 export class GrantDetailsComponent implements OnInit {
-  constructor() {  }
+  constructor(private route: ActivatedRoute) {  }
 
-  ngOnInit() {}
+  grantUrl: string;
+
+  ngOnInit() {
+    this.getGrantDetails();
+  }
+
+  getGrantDetails() {
+    const grantUrl = this.route.snapshot.paramMap.get('url');
+    this.grantUrl = grantUrl;
+  }
 }

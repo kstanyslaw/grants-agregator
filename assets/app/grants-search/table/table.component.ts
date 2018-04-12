937cc1f6cdc98cb27313a5663f6d8e1a8ca428e2
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2018-04-12 13:35:32 +0700

    Add redirect to selected grant

--- assets/app/grants-search/table/table.component.ts
+++ assets/app/grants-search/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Grant } from '../manualy-add/grant.model';
 // import { TableService } from './table.service';
@@ -7,13 +8,14 @@ import { ManualyAddService } from '../manualy-add/manualy-add.service';
 @Component({
   selector: 'table-app',
   templateUrl: 'table.component.html',
+  styleUrls: ['table.component.css']
 })
 export class TableComponent implements OnInit {
 
   grants: Grant[] = [];
 
   // constructor(private tableService: TableService) {}
-  constructor(private manualyAddService: ManualyAddService) {}
+  constructor(private manualyAddService: ManualyAddService, private router: Router) {}
 
   ngOnInit() {
     this.manualyAddService.getGrant()
@@ -23,5 +25,10 @@ export class TableComponent implements OnInit {
         // console.log(this.grants);
       }
     )
+  }
+
+  onClick(url) {
+    console.log(url);
+    this.router.navigateByUrl('/details/' + url);
   }
 }

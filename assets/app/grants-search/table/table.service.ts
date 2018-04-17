4b40771bf316ec37a56afdbe522e74ebc6ebd914
Stanislav <k.stanyslaw@gmail.com>
2018-04-17 18:18:27 +0700

    Add variable file & Login botton

--- assets/app/grants-search/table/table.service.ts
+++ assets/app/grants-search/table/table.service.ts
@@ -4,16 +4,19 @@ import 'rxjs/Rx';
 import {Observable} from 'rxjs';
 
 import { Grant } from '../manualy-add/grant.model';
+import { VARIABLES } from '../../../../var'
 
 @Injectable()
 export class TableService {
+
+  private variables: any = VARIABLES;
 
   private grants: Grant[] = [];
 
   constructor(private http: Http) {}
 
   getGrant() {
-    return this.http.get('http://localhost:3000/grant')
+    return this.http.get(this.variables.api + 'grant')
       .map((response: Response) => {
         const grants = response.json().obj;
         let transformedGrants: Grant[] = [];

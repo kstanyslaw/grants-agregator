2d0441cded286b9884df20424f8d41a74be592d2
Unknown <k.stanyslaw@gmail.com>
2018-04-20 12:10:11 +0700

    Add form to Login interface
    Now, when you bind your data in login and submit, it appears in console

--- assets/app/navbar/authentication/authentication.component.ts
+++ assets/app/navbar/authentication/authentication.component.ts
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
     styleUrls:['authentication.component.css']
 })
 
-export class AuthenticationComponent {
+export class AuthenticationComponent{
     
     isShow = false;
 
--- assets/app/navbar/authentication/login/login.component.ts
+++ assets/app/navbar/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component ({
     selector: 'login-app',
@@ -6,6 +7,19 @@ import { Component } from "@angular/core";
     styleUrls: ['login.component.css']
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+    loginForm: FormGroup;
+
+    onSubmit() {
+        console.log(this.loginForm.value);
+        this.loginForm.reset();
+    }
+
+    ngOnInit() {
+        this.loginForm = new FormGroup({
+            email: new FormControl(null, Validators.required),
+            password: new FormControl(null, Validators.required)
+        })
+    }
 }

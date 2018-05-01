81ca042c6754c2f244ba7863efbb6576cef7ce41
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2018-05-02 02:34:48 +0700

    Add manualy-add button some style

--- assets/app/grants-search/manualy-add/manualy-add.component.ts
+++ assets/app/grants-search/manualy-add/manualy-add.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'manualy-add-app',
     templateUrl: 'manualy-add.component.html',
-    // styleUrls: ['']
+    styleUrls: ['manualy-add.component.css']
 })
 
 export class ManualyAddComponent implements OnInit {

915094af862f5d8cea0acebbc44489bc69273ae1
Unknown <k.stanyslaw@gmail.com>
2018-05-04 01:57:46 +0700

    Greate grant details find backend and frontend
    Show details on page (in console there is error with 'name' but grant.name shows correct on the page)
    Need to change api route for details

--- assets/app/grant-details/grant-details.component.ts
+++ assets/app/grant-details/grant-details.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { GrantDetailsService } from './grant-details.service';
+import { Grant } from '../grant.model';
+
 @Component({
   selector: 'grant-details-app',
   templateUrl: 'grant-details.component.html',
 })
 export class GrantDetailsComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {  }
+  constructor(private route: ActivatedRoute, private grantDetailsService: GrantDetailsService) { }
 
-  grantId: string;
+  private grant: Grant;
 
   ngOnInit() {
     this.getGrantDetails();
@@ -17,6 +20,13 @@ export class GrantDetailsComponent implements OnInit {
 
   getGrantDetails() {
     const grantId = this.route.snapshot.paramMap.get('id');
-    this.grantId = grantId;
+    this.grantDetailsService.getGrantDetails(grantId)
+      .subscribe(
+        (grant: Grant) => {
+          this.grant = grant;
+          console.log(this.grant)
+        },
+        error => console.error(error)
+      );
   }
 }
--- routes/grant.js
+++ routes/grant.js
@@ -5,43 +5,65 @@ const Grant = require('../models/grant');
 
 // Get Grants
 router.get('/', function(req, res, next) {
-  Grant.find()
-    .exec(function(err, grants) {
-      if (err) {
-        return res.status(500).json({
-          title: 'An error occured',
-          error: err
+    Grant.find()
+        .exec(function(err, grants) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occured',
+                    error: err
+                })
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: grants
+            })
         })
-      }
-      res.status(200).json({
-        message: 'Success',
-        obj: grants
-      })
+})
+
+// Find one Grant by id 
+router.get('/:id', function(req, res, next) {
+    Grant.findById(req.params.id, function(err, grant) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            })
+        }
+        if (!grant) {
+            return res.status(500).json({
+                title: 'No grants found',
+                error: err
+            })
+        }
+        res.status(200).json({
+            message: 'Success',
+            obj: grant
+        })
     })
 })
 
 // Save Grant
 router.post('/', function(req, res, next) {
-  var grant = new Grant({
-    name: req.body.name,
-    description: req.body.description,
-    price: req.body.price,
-    url: req.body.url,
-    categories: req.body.categories,
-    duration: req.body.duration,
-  });
-  grant.save(function(err, result) {
-    if (err) {
-      return res.status(500).json({
-        title: 'An error occured',
-        error: err
-      })
-    }
-    res.status(201).json({
-      message: 'Saved grant',
-      obj: result
+    var grant = new Grant({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        url: req.body.url,
+        categories: req.body.categories,
+        duration: req.body.duration,
+    });
+    grant.save(function(err, result) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            })
+        }
+        res.status(201).json({
+            message: 'Saved grant',
+            obj: result
+        })
     })
-  })
 })
 
 module.exports = router;

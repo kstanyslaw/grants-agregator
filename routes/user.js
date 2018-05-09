583a3b073d6ab3d4f4e4991f9d34d3449e1e2c9f
Unknown <k.stanyslaw@gmail.com>
2018-05-09 18:41:03 +0700

    Enable encrypts
    App encrypts user password with bcrypt and send jwt token do frontend (localStorage) when user ligdin' in

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 var router = express.Router();
+
 var sendEmail = require('./email/email-send');
 
 const User = require('../models/user');
@@ -19,13 +22,13 @@ router.post('/login', function(req, res, next) {
                 error: { message: "User could not be found" }
             })
         }
-        if (req.body.password != user.password) {
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
             return res.status(401).json({
                 title: "login failed",
                 error: { message: "Invalid password" }
             })
         }
-        var token = { user: "logged in" }; // Need encryption
+        var token = jwt.sign({user: user}, 'GQ9UPbT3m3VNfgYd', {expiresIn: 7200});
         res.status(200).json({
             message: "Succesfully logged in",
             token: token,
@@ -39,7 +42,7 @@ router.post('/login', function(req, res, next) {
 router.post('/', function(req, res, next) {
     var user = new User({
         email: req.body.email,
-        password: req.body.password,
+        password: bcrypt.hashSync(req.body.password, 10),
         role: 'tempUser'
     });
     user.save(function(err, result) {
@@ -119,35 +122,6 @@ router.post('/check-email', function(req, res, next) {
         res.status(200).json({
             title: "Email is free",
             message: "You can SingUp with this email"
-        })
-    })
-})
-
-// Check role for grant-add button
-router.post('/check-role', function(req, res, next) {
-    User.findById(req.body.userId, function(err, user) {
-        if (err) {
-            return res.status(500).json({
-                title: "An Error Occured",
-                error: err
-            })
-        }
-        if (!user) {
-            return res.status(401).json({
-                title: "No user found",
-                error: { message: "User could not be found" }
-            })
-        }
-        if (user.role != 'administrator') {
-            return res.status(401).json({
-                title: "Role isn't appropriate",
-                error: {message: "Permission denied"},
-                result: false
-            })
-        }
-        res.status(200).json({
-            title: "Role is appropriate",
-            result: true
         })
     })
 })
